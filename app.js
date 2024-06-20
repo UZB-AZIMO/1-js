fd7f976bd256242a25c6a594c0a7db5de7e818f6
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-06-21 01:37:25 +0500

    js

---
+++ app.js
@@ -0,0 +1,64 @@
+// // {1-MISOL
+ 
+// let a = 15
+// let b = 4
+// let i = 0
+// while (a>b) {
+//     a-=b
+//     i++
+// }
+// console.log (i);
+
+// // }
+
+
+// {2-MISOL
+
+// let a = 12
+// let b = 5
+// let i = 0
+// while (a > b){
+//     a-=b
+//     ++i
+// }
+// console.log (i);
+
+// }
+
+
+
+// {3-misol
+
+//     let n = 12 
+//     let k = 5
+//     let i = 0
+//     let b = 0
+//     while (n >= k){
+//             n-=k
+//             ++i
+//         }
+
+// console.log(i);
+
+// console.log(b);
+
+// }
+
+
+// {4-misol
+
+// let n = 20
+// let i=0
+// while (n>=0) {
+//     n-=3
+//     ++i
+// }
+
+// console.log(i);
+
+// }
+
+
+
+
+
